fe9de86c0149be8cd0d7446372fd84a336ce4e5d
Juan Manuel Garcia <juan@localhost.localdomain>
2020-07-09 14:35:34 -0300

    fixes

--- variables.tf
+++ variables.tf
@@ -9,40 +9,12 @@ variable "values" {
 
 variable "letsencrypt_email" {}
 
-variable "azure_service_principal_id" { 
-default = ""
-}
-
-variable "azure_client_secret" {
-	default = ""
-}
-
 variable "cloudflare_apikey" {
-	default = ""
-}
-
-variable "azure_subscription_id" {
-	default = ""
-}
-
-variable "azure_tenant_id" {
-	default = ""
-}
-
-variable "azure_resource_group_name" { 
-default = ""
-}
-
-variable "azure_zone_name" {
 	default = ""
 }
 
 variable "cloudflare_email" {
 	default = ""
-}
-
-variable "solver" {
-	default = "cloudflare"
 }
 
 variable "dependencies" {
